e2cc3dd0bc7e2b687a2f0ba711d749fd84b2d16f
Yusuf Akin <kutluhanakin@gmail.com>
2022-09-23 13:07:50 -0400

    button

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -9,7 +9,9 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button></button>
     </div>
+    
   );
 }
 
